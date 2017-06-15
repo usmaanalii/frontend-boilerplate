1e5f4b65214959f655f0fa5eadbcbf10f86d689e
usyyy <usmaanalii@hotmail.co.uk>
2017-06-15 18:19:35 +0100

    .

--- gulpfile.js
+++ gulpfile.js
@@ -28,11 +28,11 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     watchify = require('watchify'),
     browserify = require('browserify'),
-    exorcist    = require('exorcist'),
+    exorcist = require('exorcist'),
     source = require('vinyl-source-stream'),
     buffer = require('vinyl-buffer'),
     assign = require('lodash.assign'),
-    babelify    = require('babelify'),
+    babelify = require('babelify'),
 
     imageResize = require('gulp-image-resize'),
     imagemin = require('gulp-imagemin'),
@@ -90,7 +90,6 @@ var onError = function(err) {
  ****************************************************
  */
 
-
 /**
  ****************************************************
  * Server and browser reload tasks
@@ -107,7 +106,7 @@ gulp.task('browserSync', ['bundle'], function() {
     browserSync.init({
         server: {
             baseDir: htmlDest
-        },
+        }
     });
 });
 
@@ -118,36 +117,21 @@ gulp.task('browserSync', ['bundle'], function() {
  */
 
 gulp.task('html-partial', function() {
-    return gulp.src([htmlFiles])
-        .pipe(htmlPartial({
-            basePath: 'src/html/partials/'
-        }))
-        .pipe(gulp.dest(htmlDest))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+    return gulp.src([htmlFiles]).pipe(htmlPartial({basePath: 'src/html/partials/'})).pipe(gulp.dest(htmlDest)).pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('html-minify', function() {
-    return gulp.src('dist/**/*.html')
-        .pipe(htmlmin({
-            collapseWhitespace: true
-        }))
-        .pipe(gulp.dest(htmlDest));
+    return gulp.src('dist/**/*.html').pipe(htmlmin({collapseWhitespace: true})).pipe(gulp.dest(htmlDest));
 });
 
 gulp.task('html-remove-partials', function() {
     console.log('Removing partials from dist directory');
-    return gulp.src('dist/partials', {
-            read: false
-        })
-        .pipe(clean());
+    return gulp.src('dist/partials', {read: false}).pipe(clean());
 });
 
 gulp.task('html-to-root', [], function() {
     console.log('Moving html to root');
-    gulp.src('dist/**/*.html')
-        .pipe(gulp.dest(''));
+    gulp.src('dist/**/*.html').pipe(gulp.dest(''));
 });
 
 /**
@@ -158,34 +142,12 @@ gulp.task('html-to-root', [], function() {
 
 gulp.task('sass-compile', function() {
     console.log('Compiling SASS including autoprefixer, sourcemaps and bulk imports');
-    return gulp.src('src/sass/main.sass')
-        .pipe(sourcemaps.init())
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(bulkSass())
-        .pipe(
-            sass({
-                includePaths: ['src/sass']
-            }))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(rename('main.min.css'))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest(cssDest))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+    return gulp.src('src/sass/main.sass').pipe(sourcemaps.init()).pipe(plumber({errorHandler: onError})).pipe(bulkSass()).pipe(sass({includePaths: ['src/sass']})).pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false})).pipe(rename('main.min.css')).pipe(cleanCSS()).pipe(gulp.dest(cssDest)).pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('css-purify', function() {
     console.log('Removing unused CSS');
-    return gulp.src('dist/css/main.min.css')
-        .pipe(purify([jsFiles, htmlFiles]))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest(cssDest));
+    return gulp.src('dist/css/main.min.css').pipe(purify([jsFiles, htmlFiles])).pipe(cleanCSS()).pipe(gulp.dest(cssDest));
 });
 
 /**
@@ -196,20 +158,12 @@ gulp.task('css-purify', function() {
 
 gulp.task('js-purify', function() {
     console.log('Minifying all js files apart form jquery (which is already minified)');
-    return gulp.src(['!src/js/lib/jquery.min.js', jsFiles])
-        .pipe(gulp.dest('src/js'))
-        .pipe(uglify());
+    return gulp.src(['!src/js/lib/jquery.min.js', jsFiles]).pipe(gulp.dest('src/js')).pipe(uglify());
 });
 
 gulp.task('js-concat', function() {
     console.log('Concatenating all js files and renaming it to main.min.js');
-    return gulp.src(['src/js/lib/jquery.min.js', jsFiles])
-        .pipe(concat('main.js'))
-        .pipe(rename('main.min.js'))
-        .pipe(gulp.dest(jsDest))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+    return gulp.src(['src/js/lib/jquery.min.js', jsFiles]).pipe(concat('main.js')).pipe(rename('main.min.js')).pipe(gulp.dest(jsDest)).pipe(browserSync.reload({stream: true}));
 });
 
 ///////////////////////////////////////////
@@ -221,9 +175,7 @@ watchify.args.debug = true;
 var bundler = watchify(browserify('./src/js/app.js', watchify.args));
 
 // Babel transform
-bundler.transform(babelify.configure({
-    sourceMapRelative: 'app/js'
-}));
+bundler.transform(babelify.configure({sourceMapRelative: 'app/js'}));
 
 // On updates recompile
 bundler.on('update', bundle);
@@ -232,30 +184,23 @@ function bundle() {
 
     gutil.log('Compiling JS...');
 
-    return bundler.bundle()
-        .on('error', function (err) {
-            gutil.log(err.message);
-            browserSync.notify("Browserify Error!");
-            this.emit("end");
-        })
-        .pipe(exorcist('.dist/js/bundle.js.map'))
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('./dist/js'))
-        .pipe(browserSync.stream({once: true}));
+    return bundler.bundle().on('error', function(err) {
+        gutil.log(err.message);
+        browserSync.notify("Browserify Error!");
+        this.emit("end");
+    }).pipe(exorcist('.dist/js/bundle.js.map')).pipe(source('bundle.js')).pipe(gulp.dest('./dist/js')).pipe(browserSync.stream({once: true}));
 }
 
 /**
  * Gulp task alias
  */
-gulp.task('bundle', function () {
+gulp.task('bundle', function() {
     return bundle();
 });
 
 gulp.task('js-bundle-minify', function() {
     console.log('Minifying the bundle');
-    return gulp.src('dist/js/bundle.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+    return gulp.src('dist/js/bundle.js').pipe(uglify()).pipe(gulp.dest('dist/js'));
 });
 
 /**
@@ -265,31 +210,24 @@ gulp.task('js-bundle-minify', function() {
  */
 gulp.task('sprite-generator', function() {
     // Generate our spritesheet
-    var spriteData = gulp.src('dist/img/*.png').pipe(spritesmith({
-        imgName: 'sprite.png',
-        cssName: '_sprite-sheet.sass',
-    }));
+    var spriteData = gulp.src('dist/img/*.png').pipe(spritesmith({imgName: 'sprite.png', cssName: '_sprite-sheet.sass'}));
 
     // Pipe image stream through image optimizer and onto disk
     var imgStream = spriteData.img
-        // DEV: We must buffer our stream into a Buffer for `imagemin`
-        // .pipe(buffer())
-        // .pipe(imagemin())
+    // DEV: We must buffer our stream into a Buffer for `imagemin`
+    // .pipe(buffer())
+    // .pipe(imagemin())
         .pipe(gulp.dest('dist/css'));
 
     // Pipe CSS stream through CSS optimizer and onto disk
-    var cssStream = spriteData.css
-        .pipe(gulp.dest('src/sass/helpers'));
+    var cssStream = spriteData.css.pipe(gulp.dest('src/sass/helpers'));
 
     var spriteSass = {
         spriteImport: '@import \'_sprite-sheet\'',
         spriteInclude: '@include sprites($spritesheet-sprites)'
     };
 
-    var makeSpriteSheet = file('_sprite.sass', spriteSass.spriteImport + '\n\n' + spriteSass.spriteInclude, {
-            src: true
-        })
-        .pipe(gulp.dest('src/sass/helpers'));
+    var makeSpriteSheet = file('_sprite.sass', spriteSass.spriteImport + '\n\n' + spriteSass.spriteInclude, {src: true}).pipe(gulp.dest('src/sass/helpers'));
 
     // Return a merged stream to handle both `end` events
     console.log('Creating sprite.png and inserting it into dist/css');
@@ -300,24 +238,13 @@ gulp.task('sprite-generator', function() {
 gulp.task('img-min', function() {
     console.log('Minfication of images to reduce size');
     gulp.src('src/img/*')
-        // Caching images that ran through imagemin
-        .pipe(cache(imagemin({
-            verbose: true,
-            interlaced: true
-        })))
-        .pipe(gulp.dest('dist/img'));
+    // Caching images that ran through imagemin
+        .pipe(cache(imagemin({verbose: true, interlaced: true}))).pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('img-resize', function() {
     console.log('Reducing image to specified size');
-    gulp.src('dist/img/image-2.png')
-        .pipe(imageResize({
-            width: 200,
-            height: 200,
-            crop: true,
-            upscale: false
-        }))
-        .pipe(gulp.dest('dist/img'));
+    gulp.src('dist/img/image-2.png').pipe(imageResize({width: 200, height: 200, crop: true, upscale: false})).pipe(gulp.dest('dist/img'));
 });
 /**
  ****************************************************
@@ -334,8 +261,7 @@ gulp.task('img-resize', function() {
  */
 gulp.task('deploy', function() {
     console.log('Creates a gh pages branch and deploys to github pages from the dist directory');
-    return gulp.src("dist/**/*")
-        .pipe(deploy());
+    return gulp.src("dist/**/*").pipe(deploy());
 });
 
 /**
